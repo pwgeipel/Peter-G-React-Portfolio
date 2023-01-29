37fe9acd2bed6cd99f14e4e61a4cf632e813f433
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-01-29 16:14:37 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { useState } from 'react'
+
+import Navbar from './components/navbar'
+import AboutPage from './pages/about'
+import PortfolioPage from './pages/portfolio'
+import ContactPage from './pages/contact'
+import ResumePage from './pages/resume'
+
+
 
 
 function App() {
+  const [view, setView] = useState('about')
+
+  const renderView = () => {
+    if (view === 'about') {
+      return <AboutPage />
+    } else if ( view === 'portfolio') {
+      return <PortfolioPage />
+    } else if (view === 'contact') {
+      return <ContactPage />
+    } else if (view === 'resume') {
+      return <ResumePage />
+    } else {
+      return 'No matching view found!'
+    }
+  }
+
   return (
-    null
+    <>
+     <Navbar 
+      view={view}
+      setView={setView}
+      />
+     { renderView() }
+    </>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,44 @@
-const Navbar = props => {
-    return 'Navbar'
+const Navbar = ({ view, setView }) => {
+
+    console.log(view, setView)
+    return (
+        <nav className="navbar navbar-expand-lg bg-primary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Peter Geipel's Portfolio</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <a className="nav-link active" href="#"
+                        >
+                            <i className="bi bi-person-circle"></i> About
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#"
+                        >
+                            <i className="bi bi-palette-fill"></i> Portfolio
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#"
+                        >
+                            Contact
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link"
+                        >
+                            Resume
+                        </a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            </nav>
+    )
 }
 
 export default Navbar

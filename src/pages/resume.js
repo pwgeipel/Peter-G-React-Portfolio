b454858fa087eb3f6e8f9b5c6205af9cebb3e222
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-01-31 17:26:40 -0600

    resume jsx

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,5 +1,28 @@
-const ResumePage = props => {
-    return 'ResumePage'
+function ResumePage() {
+    return (
+        <section>
+            <h1>Resume</h1>
+                <h3>
+                    Front-End Skills
+                </h3>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                        <li>Bootstrap</li>
+                        <li>JQuery</li>
+                        <li>React</li>
+                    </ul>
+                <h3>Back-End Skills</h3>
+                    <ul>
+                    <li>Node</li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    </ul>
+        </section>
+    );
 }
 
-export default ResumePage
+export default ResumePage;

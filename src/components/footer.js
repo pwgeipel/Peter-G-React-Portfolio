3b43194b0532d0e602c21261ee80a710d000547f
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-01-31 16:29:46 -0600

    footerstays on bottom

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,27 @@
-const Footer = props => {
-    return 'Footer'
+import '../index.css';
+
+function Footer() {
+
+    return (
+        <footer className="bg-light text-center footer">
+            <div className="container p-4">
+                <section className="mb-4">
+                <a href="https://github.com/pwgeipel">
+                    <i className="bi bi-github"></i>             
+                </a>
+                <a href="https://www.linkedin.com/in/peter-geipel-75a84795/">
+                    <i className="bi bi-linkedin"></i>             
+                </a>
+                <a href="https://stackoverflow.com/users/19553134/peter-geipel">
+                    <i className="bi bi-stack-overflow"></i>            
+                </a>
+
+                </section>
+
+            </div>
+
+        </footer>
+    )
 }
 
 export default Footer

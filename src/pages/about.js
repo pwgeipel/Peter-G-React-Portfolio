fd543ed21e78582d53a8148b59fd68b58c0717f7
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-01-31 15:04:01 -0600

    about bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,6 +11,7 @@ const AboutPage = props => {
             <div>
                 <img src={selfieImage} width={250} height={250} alt="Selfie" />
             </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec vero pietas adversus deos nec quanta iis gratia debeatur sine explicatione naturae intellegi potest. Quod si ita se habeat, non possit beatam praestare vitam sapientia. Haec para/doca illi, nos admirabilia dicamus. Compensabatur, inquit, cum summis doloribus laetitia. Duo Reges: constructio interrete. Peccata paria. Bonum incolumis acies: misera caecitas. Quia dolori non voluptas contraria est, sed doloris privatio. Erat enim res aperta. Quia nec honesto quic quam honestius nec turpi turpius. Dempta enim aeternitate nihilo beatior Iuppiter quam Epicurus; Qui non moveatur et offensione turpitudinis et comprobatione honestatis? </p>
             </Container>
     )
 }

87e2d3795c19ea3aca093b227b0c44c80ac27f3f
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-01-31 22:11:07 -0600

    resume page download link

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,27 +1,36 @@
+import Container from "../components/container";
+
 function ResumePage() {
     return (
-        <section>
-            <h1>Resume</h1>
-                <h3>
-                    Front-End Skills
-                </h3>
-                    <ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>Javascript</li>
-                        <li>Bootstrap</li>
-                        <li>JQuery</li>
-                        <li>React</li>
-                    </ul>
-                <h3>Back-End Skills</h3>
-                    <ul>
-                    <li>Node</li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    </ul>
-        </section>
+        <Container className="mt-5">
+            <section>
+                <h1>Resume</h1>
+                    <p>Download resume 
+                        <a href="../public/sampleResume.pdf" download={true}>
+                            here
+                        </a>
+                    </p>
+                    <h3>
+                        Front-End Skills
+                    </h3>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>Javascript</li>
+                            <li>Bootstrap</li>
+                            <li>JQuery</li>
+                            <li>React</li>
+                        </ul>
+                    <h3>Back-End Skills</h3>
+                        <ul>
+                        <li>Node</li>
+                        <li>API</li>
+                        <li>Express</li>
+                        <li>Mongoose</li>
+                        <li>MySQL</li>
+                        </ul>
+            </section>
+        </Container>
     );
 }
 

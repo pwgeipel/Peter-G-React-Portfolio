fbab80dfb588c2ea90b042e3e93f20ed9038435d
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-02-04 14:05:51 -0600

    improvements to portolio page

--- src/components/project.js
+++ src/components/project.js
@@ -1,5 +1,43 @@
-function Project () {
-    
-}
+// function Project () {
+export const projects = [
+        {
+            title: "Shroomies",
+            subtitle: "A Full-stack App",
+            image: "./assets/morel (1).jpeg",
+            link: "#",
+            github: "#"
+        },{
+            title: "Project 2",
+            subtitle: "A Full-stack App",
+            image: "./assets/cookies.jpeg",
+            link: "#",
+            github: "#"
+        },{
+            title: "Project 3",
+            subtitle: "A Full-stack App",
+            image: "./assets/onions.jpeg",
+            link: "#",
+            github: "#"
+        },{
+            title: "Project 4",
+            subtitle: "A Full-stack App",
+            image: "./assets/pretzel.jpeg",
+            link: "#",
+            github: "#"
+        },{
+            title: "Project 5",
+            subtitle: "A Full-stack App",            
+            image: "./assets/macarons-small.jpeg",
+            link: "#",
+            github: "#"
+        },{
+            title: "Project 6",
+            subtitle: "A Full-stack App",            
+            image: "./assets/320.png",
+            link: "#",
+            github: "#"
+        },
+    ]
+// }
 
-export default Project
+// export default Project
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,14 +1,37 @@
 import Container from "../components/container";
+import { projects } from "../components/project"
 
 
 function PortfolioPage() {
     return (
-        <Container className="mt-5">
-            <h1>Portfolio</h1>
-            <div className="flex-row">
+        // <Container className="mt-5">
+        //     <h1>Portfolio</h1>
+        //     <div className="flex-row">
 
-            </div>
-        </Container>
+        //     </div>
+        // </Container>
+        <div className="flex flex-wrap -m-4">
+          {projects.map((project) => (
+            <a
+              href={project.link}
+              key={project.image}
+              className="col-6 col-sm-12">
+              <div className="flex-row">
+                <img
+                  src={project.image}
+                />
+                <div className="fs-2">
+                  <h2 className="fs-4">
+                    {project.subtitle}
+                  </h2>
+                  <h1 className="fs-3">
+                    {project.title}
+                  </h1>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
     )
 }
 
